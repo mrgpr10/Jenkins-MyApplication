a7854ea9ffcca720861e4f870b845f60de18d8fe
Guruprasad Raikar <gp.raikar@planview.com>
2024-08-28 14:59:40 +0530

    Add .gitignore and clean up unwanted files

---
+++ MyApp.Tests/CalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp;
+
+namespace MyApp.Tests
+{
+    public class CalculatorTest
+    {
+        [Test]
+        public void Add_Test()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            //Act
+            int result = calculator.Add(2, 3);
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
+
+    }
+}
